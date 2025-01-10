48faccafa58a69ffd124b7404502150813c72e43
Guoxuan Xu <guoxuan.xu8@gmail.com>
2025-01-09 17:49:39 -0800

    Adding Cell Parser function structure

--- README.md
+++ README.md
@@ -32,7 +32,7 @@
 <!-- ABOUT THE PROJECT -->
 ## About the dataset
 
-[![Product Name Screen Shot][product-screenshot]](https://example.com)
+<!-- [![Product Name Screen Shot][product-screenshot]](https://example.com) -->
 
 About this dataset:
 The dataset can be found [here](http://xteam.xbio.top/CellMarker/download.jsp). These records contain data about molecules expressed on the surface, within, or secreted by cells. These markers are used to identify and classify the types, states, or functions of cells within a population. 
--- data_parser.py
+++ data_parser.py
@@ -8,39 +8,78 @@ import re
 import pandas as pd
 
 # from biothings import config
-from biothings.utils.dataload import dict_convert
+from biothings.utils.dataload import dict_convert, dict_sweep
 
-# logging = config.logger
+# logging = config.loggers
+
+
+def pairUp_seq_info(value_dict: dict) -> list:
+    """pair up all the element in each value of the dictionary
+
+    Args:
+        value_dict (dict): values that needs to be paired up
+
+    Return:
+        a list of dictionary where the each dict is a pairings
+    """
+    print(value_dict)
 
 
 def load_annotations(data_folder):
+    """Converting data into expected JSON format
+
+    Args:
+        data_folder (str): the relative data path to the data source
+
+    Return:
+        the yield JSON data
+    """
+
     # convert and check if the data file exist
-    infile = os.path.join(data_folder, "all_cell_markers.tsv")
-    assert os.path.exists(infile)
+    infile_all_cell = os.path.join(data_folder, "all_cell_markers.txt")
+    infile_single_cell = os.path.join(data_folder, "Single_cell_markers.txt")
 
-    # #
-    # dat = (
-    #     pd.read_csv(infile, sep="\t", quoting=csv.QUOTE_NONE)
-    #     .squeeze("columns")
-    #     .to_dict(orient="records")
-    # )
-    # results = {}
-    # for rec in dat:
-    #     if not rec["Gene"] or pd.isna(rec["Gene"]):
-    #         logging.warning(
-    #             "No gene information for annotation ID '%s'", rec["Annotation ID"]
-    #         )
-    #         continue
-    #     _id = re.match(".* \((.*?)\)", rec["Gene"]).groups()[0]
-    #     # we'll remove space in keys to make queries easier. Also, lowercase is preferred
-    #     # for a BioThings API. We'll an helper function from BioThings SDK
-    #     process_key = lambda k: k.replace(" ", "_").lower()
-    #     rec = dict_convert(rec, keyfn=process_key)
-    #     results.setdefault(_id, []).append(rec)
+    assert os.path.exists(infile_all_cell)
+    assert os.path.exists(infile_single_cell)
 
-    # for _id, docs in results.items():
-    #     doc = {"_id": _id, "annotations": docs}
-    #     yield doc
+    # import data and store them in a list
+    data = []
+    with open(infile_all_cell, mode="r") as file:
+        reader = csv.DictReader(file, delimiter="\t", quoting=csv.QUOTE_NONE)
+        data += [row for row in reader]
+    with open(infile_single_cell, mode="r") as file:
+        reader = csv.DictReader(file, delimiter="\t", quoting=csv.QUOTE_NONE)
+        data += [row for row in reader]
+
+    results = dict()
+
+    for record in data:
+        # converting all the key to standard format
+        process_key = lambda k: k.replace(" ", "_").lower()
+        record = dict_convert(record, keyfn=process_key)
+
+        if record["geneid"] == "NA":
+            continue
+
+        _id = record["geneid"]
+        # cell_marker = record["cellMarker"]
+        results.setdefault(_id, []).append(record)
+
+        # zip these elements together to get multiple copies
+        ZIP_COLUMNS = ["cellmarker", "genesymbol", "geneid"]
+
+        for gene_expression in pairUp_seq_info(
+            {key: record[key] for key in record if key in ZIP_COLUMNS}
+        ):
+            print(gene_expression)
+            return
+
+    for _id, docs in results.items():
+        doc = {"_id": _id, "cellMarker": docs}
+        yield doc
+
+    # return results
 
 
-load_annotations("data")
+if __name__ == "__main__":
+    load_annotations("data")
