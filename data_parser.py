a56ffb8a630b426d0dc6c720d1c4a62d294760c8
Guoxuan Xu <guoxuan.xu8@gmail.com>
2025-01-16 21:36:06 -0800

    Update data_parser.py
    Resolving the mismatch issue

--- data_parser.py
+++ data_parser.py
@@ -1,26 +1,51 @@
 # this file will parse the data into json
 
 import csv
+import json
 import os
 import re
 
 import numpy as np
 import pandas
-
-# from biothings import config
+import requests
+from biothings import config
 from biothings.utils.dataload import dict_convert, dict_sweep
 
-# logging = config.logger
+logging = config.logger
 GENE_ID = "geneid"
 GENE_SYMBOL = "genesymbol"
 MARKER_RESOURCE = "markerresource"
 CELL_INFO_KEYS = [
-    "cellontologyid", "cellname", "celltype", "cancertype", 
-    "tissuetype", "uberonontologyid", "speciestype", 
-    "markerresource", "pmid", "company"
+    "cellontologyid",
+    "cellname",
+    "celltype",
+    "cancertype",
+    "tissuetype",
+    "uberonontologyid",
+    "speciestype",
+    "markerresource",
+    "pmid",
+    "company",
 ]
 ZIP_COLUMNS = [GENE_ID, GENE_SYMBOL]
 FILES = ["all_cell_markers.txt", "Single_cell_markers.txt"]
+
+
+def get_gene_symbol(gene_ids: str) -> str:
+    """The function that make an API request to my mygene.info, requesting for symbol using gene ID
+
+    Args:
+        gene_id (str): the id of the gene
+
+    Returns:
+        (str) the symbol of the gene
+    """
+    headers = {"content-type": "application/x-www-form-urlencoded"}
+    params = f"ids={gene_ids}&fields=symbol"
+    res = requests.post("http://mygene.info/v3/gene", data=params, headers=headers)
+
+    return json.loads(res.content.decode("utf-8"))
+
 
 def str_to_list(listLikeStr: str) -> list:
     """Case str-like-list into actual list, nested list will be expand
@@ -46,6 +71,7 @@ def str_to_list(listLikeStr: str) -> list:
     parsed_str_list = parsed_str.split(",")
     return [val.strip() for val in parsed_str_list]
 
+
 def pairUp_seq_info(value_dict: dict) -> list:
     """Pair up all the elements in each value of the dictionary
 
@@ -60,12 +86,24 @@ def pairUp_seq_info(value_dict: dict) -> list:
     >>> pairUp_seq_info({"a": '1'})
     [{'a': '1'}]
     """
-    
     keys = list(value_dict.keys())
     values = [str_to_list(value) for value in value_dict.values()]
 
-    # if not all(len(v) == len(next(iter(values))) for v in values):
-    #     raise ValueError("All lists in value_dict must have the same length")
+    if not all(len(v) == len(next(iter(values))) for v in values):
+        print("..................")
+        gene_ids = ", ".join(
+            [
+                gene_id
+                for gene_id in str_to_list(value_dict["geneid"])
+                if gene_id.casefold() != "na" and gene_id != ""
+            ]
+        )
+        symbols = get_gene_symbol(gene_ids)
+
+        return [
+            {"geneid": gene_info["_id"], "genesymbol": gene_info["symbol"]}
+            for gene_info in symbols
+        ]
 
     return [dict(zip(keys, combination)) for combination in zip(*values)]
 
@@ -75,8 +113,10 @@ def make_uniqueMarker(cellMarkers: list) -> list:
     cellMarkers = list({tuple(sorted(marker.items())) for marker in cellMarkers})
     return [dict(marker) for marker in cellMarkers]
 
+
 def select_items(record, item_keys):
     return {key: record[key] for key in item_keys if key in record}
+
 
 def load_data_files(data_folder: str, files: list) -> list:
     """
@@ -99,6 +139,7 @@ def load_data_files(data_folder: str, files: list) -> list:
             data.extend(reader)
     return data
 
+
 def load_annotations(data_folder):
     """Converting data into expected JSON format
 
@@ -118,68 +159,64 @@ def load_annotations(data_folder):
         record = dict_convert(record, keyfn=lambda k: k.replace(" ", "_").lower())
 
         # ignore geneID that is missing
-        if record[GENE_ID].casefold() == "na" or record[GENE_ID].casefold() == '':
+        if record[GENE_ID].casefold() == "na" or record[GENE_ID].casefold() == "":
             continue
-
         # zip these elements together to get multiple copies
-        try: 
-            for gene_expression in pairUp_seq_info(select_items(record, ZIP_COLUMNS)):
-                _id = gene_expression["geneid"]
-                if _id.casefold() == "na" or _id.casefold() == "":
-                    continue
-                results.setdefault(_id, {})
-                gene_expression_dict = results[_id]
-                gene_expression_dict['symbol'] = gene_expression['genesymbol']
-                if record['markerresource'].casefold() != 'company':
-                    gene_expression_dict.setdefault('geneRelatedCells', []).append(
-                        dict_sweep({
-                            "CellOntologyID": record['cellontologyid'],
-                            "cellName": record['cellname'],
-                            "cellType": record['celltype'],
-                            "cancerType": record['cancertype'],
-                            "tissueType": record['tissuetype'],
-                            "UberonOntologyID": record['uberonontologyid'],
-                            "speciesType": record['speciestype'],
-                            "markerResource": record['markerresource'],
-                            # "PMID": record['pmid']
-                        })
+        for gene_expression in pairUp_seq_info(select_items(record, ZIP_COLUMNS)):
+            _id = gene_expression["geneid"]
+            if _id.casefold() == "na" or _id.casefold() == "":
+                continue
+            results.setdefault(_id, {})
+            gene_expression_dict = results[_id]
+            gene_expression_dict["symbol"] = gene_expression["genesymbol"]
+            if record["markerresource"].casefold() != "company":
+                gene_expression_dict.setdefault("geneRelatedCells", []).append(
+                    dict_sweep(
+                        {
+                            "CellOntologyID": record["cellontologyid"],
+                            "cellName": record["cellname"],
+                            "cellType": record["celltype"],
+                            "cancerType": record["cancertype"],
+                            "tissueType": record["tissuetype"],
+                            "UberonOntologyID": record["uberonontologyid"],
+                            "speciesType": record["speciestype"],
+                            "markerResource": record["markerresource"],
+                            "PMID": record["pmid"],
+                        }
                     )
-                else:
-                    gene_expression_dict.setdefault('geneRelatedCells', []).append(
-                        dict_sweep({
-                            "CellOntologyID": record['cellontologyid'],
-                            "cellName": record['cellname'],
-                            "cellType": record['celltype'],
-                            "cancerType": record['cancertype'],
-                            "tissueType": record['tissuetype'],
-                            "UberonOntologyID": record['uberonontologyid'],
-                            "speciesType": record['speciestype'],
-                            "markerResource": record['markerresource'],
-                            # "Company": record['company']
-                        })
+                )
+            else:
+                gene_expression_dict.setdefault("geneRelatedCells", []).append(
+                    dict_sweep(
+                        {
+                            "CellOntologyID": record["cellontologyid"],
+                            "cellName": record["cellname"],
+                            "cellType": record["celltype"],
+                            "cancerType": record["cancertype"],
+                            "tissueType": record["tissuetype"],
+                            "UberonOntologyID": record["uberonontologyid"],
+                            "speciesType": record["speciestype"],
+                            "markerResource": record["markerresource"],
+                            "Company": record["company"],
+                        }
                     )
-        except ValueError as e:
-            print('problematic record')
-            print(record)
-            continue
-
-            
+                )
     for _id, related_info in results.items():
         yield {
             "_id": _id,
-            "symbol": related_info['symbol'],
-            "geneRelatedCells": make_uniqueMarker(related_info['geneRelatedCells'])
+            "symbol": related_info["symbol"],
+            "geneRelatedCells": make_uniqueMarker(related_info["geneRelatedCells"]),
         }
 
-if __name__ == "__main__":
-    import doctest
 
-    doctest.testmod()
+if __name__ == "__main__":
+    # import doctest
+
+    # doctest.testmod()
     x = load_annotations("data")
 
-    y = [i['_id'] for i in x]
+    y = [i for i in x]
     breakpoint()
     # print(y)
 
     # print(str_to_list("Intestinal Alkaline Phosphatase"))
-
